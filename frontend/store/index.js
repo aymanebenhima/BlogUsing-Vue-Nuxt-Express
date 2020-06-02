0cde0ae72bb2bf75c36c8f00543db77c41a98ecc
Aymane Benhima <aymanebenhima@gmail.com>
2020-06-02 23:10:18 +0100

    create state management

---
+++ frontend/store/index.js
@@ -0,0 +1,59 @@
+import Vuex from "vuex";
+import axios from 'axios'
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      loadedPosts: []
+    },
+    mutations: {
+      setPosts(state, posts) {
+        state.loadedPosts = posts;
+      }
+    },
+    actions: {
+      nuxtServerInit(vuexContext, context) {
+         /* return axios.get(process.env.baseUrl + "/posts.json")
+            .then(res => {
+              const postArray = []
+              for (const key in res.data) {
+                postsArray.push({ ...res.data[key], id: key })
+              }
+              vuexContext.commit('setPosts', postsArray)
+            })
+            .catch(e => context.error(e)); */
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            vuexContext.commit("setPosts", [
+              {
+                id: "1",
+                title: "First Post",
+                previewText: "This is our first post!",
+                thumbnail:
+                  "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+              },
+              {
+                id: "2",
+                title: "Second Post",
+                previewText: "This is our second post!",
+                thumbnail:
+                  "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+              }
+            ]);
+            resolve();
+          }, 1000);
+        });
+      },
+      setPosts(vuexContext, posts) {
+        vuexContext.commit("setPosts", posts);
+      }
+    },
+    getters: {
+      loadedPosts(state) {
+        return state.loadedPosts;
+      }
+    }
+  });
+};
+
+export default createStore;
